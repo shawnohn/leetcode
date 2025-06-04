0e628f84c6ddc9d0cffb8e7996b8841839fe91ef
Shawn Ohn <developer.ohn@gmail.com>
2025-06-04 14:33:02 -0400

    35. Search Insert Position

---
+++ problems/Binary Search/35. Search Insert Position.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/search-insert-position/description/
+# Difficulty: Easy
+# tag: Array Binary Search
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+# You must write an algorithm with O(log n) runtime complexity.
+
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return left
